93b41bd0604bbad9414ad38e4cca3b2c51b9000b
tchasinga <tchasingajacques@gmail.com>
2025-04-08 11:06:56 +0300

    Remove unused imports and clean up PropertyCard component

--- src/components/PropertyCard/PropertyCard.jsx
+++ src/components/PropertyCard/PropertyCard.jsx
@@ -1,9 +1,7 @@
 import React from "react";
 import './PropertyCard.css'
-import {AiFillHeart} from 'react-icons/ai'
 import {truncate} from 'lodash'
 import { useNavigate } from "react-router-dom";
-import Heart from "../Heart/Heart";
 const PropertyCard = ({card}) => {
 
   const navigate = useNavigate();
