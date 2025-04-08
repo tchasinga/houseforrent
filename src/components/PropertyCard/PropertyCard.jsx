9aadc662e6adc769beab672a5420a8e204705067
tchasinga <tchasingajacques@gmail.com>
2025-04-08 11:09:36 +0300

    Add Heart component to PropertyCard and update layout

--- src/components/PropertyCard/PropertyCard.jsx
+++ src/components/PropertyCard/PropertyCard.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import './PropertyCard.css'
+import {AiFillHeart} from 'react-icons/ai'
 import {truncate} from 'lodash'
 import { useNavigate } from "react-router-dom";
+import Heart from "../Heart/Heart";
 const PropertyCard = ({card}) => {
 
   const navigate = useNavigate();
   return (
-    <div className="flexColStart r-card"
-    >
+    <div className="flexColStart r-card">
+      <Heart/>
       <img src={card.image} alt="home" />
       <span className="secondaryText r-price">
         <span style={{ color: "orange" }}></span>
